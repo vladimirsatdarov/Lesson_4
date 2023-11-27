0fe0f5421f92e18777f91f2e56655947ab847b96
vladimirsatdarov <150793663+vladimirsatdarov@users.noreply.github.com>
2023-11-27 06:29:38 +0100

    Update and rename softAssertionPagePresence.java to SoftAssertionPagePresence.java
    Renamed class with capital S in the beginning

--- src/test/java/github/SoftAssertionPagePresence.java
+++ src/test/java/github/SoftAssertionPagePresence.java
@@ -6,7 +6,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class softAssertionPagePresence {
+public class SoftAssertionPagePresence {
 
     @Test
     void softAssertionsCheck() {
